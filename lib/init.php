638d870fdfdb143705a28dccbdf6ee1e5485d919
Szabó Gábor <szabogabi@gmail.com>
2014-04-24 12:05:43 +0200

    piszka them support testt

--- lib/init.php
+++ lib/init.php
@@ -13,6 +13,9 @@ function roots_setup() {
 
   // Add post thumbnails (http://codex.wordpress.org/Post_Thumbnails)
   add_theme_support('post-thumbnails');
+  //add_theme_support('html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption'  ));
+  add_theme_support('post-formats', array('aside', 'gallery', 'video'));
+
   // set_post_thumbnail_size(150, 150, false);
   // add_image_size('category-thumb', 300, 9999); // 300px wide (and unlimited height)
 
