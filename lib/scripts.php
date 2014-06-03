6d41b25fb787abc5d6202912fdbca6b2e3f2898c
Szabó Gábor <szabogabi@gmail.com>
2014-06-03 16:12:10 +0200

    dropdown

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b3b6343edea59874f8f634151b0951c6');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '10600398df9b9f952588bbdf335cce1f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/contact-form.php
+++ templates/contact-form.php
@@ -1,10 +1,35 @@
 <section id="contact" class="contact collapse" >
   <div class="contact-body">
-    <h2>Online jelentkezése</h2>
-    <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.<h3>
-    <div>Név: <input type="text"></div>
-    <div>E-mail: <input type="email"></div>
-    <div>Üzenet: <textarea name="" id="" cols="30" rows="10"></textarea></div>
-    <div><input type="submit" class="btn"></div>
+    <h2>Online jelentkezés konzultációra</h2>
+    <p>*Adja meg a szükséges adatokat és kolléganőnk visszahívja Önt konzultációs időpont egyeztetésére.</p>
+    <form action="#">
+      <div class="cbal">
+        <div class="formitem">
+          <label for="name">Név*</label><input required placeholder="Kiss Erzsébet" type="text" id="name" name="name">
+        </div>
+        <div class="formitem">
+          <label for="name">Email cím*</label><input required placeholder="valaki@valahol.hu" type="email" id="email" name="email">
+        </div>
+        <div class="formitem">
+          <label for="tel">Telefonszám*</label><input required placeholder="00 12 3456789" type="tel" id="tel" name="tel">
+        </div>
+      </div>
+      <div class="cjobb">
+        <div class="formitem">
+          <label for="subject">Tárgy</label>
+          <select name="subject" id="subject" name="subject">
+            <option value="Általános érdeklődése">Általános érdeklődés</option>
+            <option value="Lorem ipsum">Általános érdeklődées</option>
+            <option value="Dolor sit amet">Dolor sit amet</option>
+          </select>
+        </div>
+        <div class="formitem">
+          <label for="message">Üzenet</label><textarea name="message" id="message" cols="30" rows="6"></textarea>
+        </div>
+      </div>
+      <div class="formactions">
+        <input type="submit" class="btn" value="Elküld">
+      </div>
+    </form>
   </div>
 </section>
