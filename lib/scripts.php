cf11bd819106ded3dee5f8dfddb8c15c22b05bdf
Szabó Gábor <szabogabi@gmail.com>
2014-06-12 14:35:16 +0200

    single post layout

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a3c37ba1ac9fc10541be10f36eca3cc2');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '95b97d4a59638079eaf6797c65267692');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/content-single.php
+++ templates/content-single.php
@@ -1,15 +1,35 @@
-<?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class(); ?>>
-    <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
-    </header>
-    <div class="entry-content">
-      <?php the_content(); ?>
-    </div>
-    <footer>
-      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
-    </footer>
-    <?php comments_template('/templates/comments.php'); ?>
-  </article>
-<?php endwhile; ?>
+<div class="subsections">
+  <aside class="subsec-sidebar">
+    <h3>Ez az oldalsav</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, reiciendis distinctio neque amet numquam beatae.
+    </p>
+    <p>
+      Expedita, illum vel saepe quia dolorem! Velit, quasi quaerat placeat quia molestias id quos nostrum!
+    </p>
+  </aside>
+  <section id="subsec-article" class="subsec">
+    <?php while (have_posts()) : the_post(); ?>
+      
+        <article <?php post_class(); ?>>
+          <header class="subsec-header">
+            <h1 class="subsec-title"><?php the_title(); ?></h1>
+            <div class="subsec-meta"><?php get_template_part('templates/entry-meta'); ?></div>
+          </header>
+          <div class="subsec-inner">
+            <div class="subsec-cont">
+              <figure class="entry-figure">
+                <?php the_post_thumbnail(); ?>
+              </figure>
+              <?php the_content(); ?>
+              <footer>
+                <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+              </footer>
+              <?php comments_template('/templates/comments.php'); ?>
+            </div>
+          </div>
+        </article>
+
+    <?php endwhile; ?>
+  </section>
+</div>
