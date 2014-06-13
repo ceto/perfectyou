44b97341f0d9a9eac572739a21ccebef5d104acd
Szabó Gábor <szabogabi@gmail.com>
2014-06-13 14:50:32 +0200

    doktor ablak sav

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4213505539671636f6eccd6827459e6e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '73c87fd07b2f214d452d5e99f428b86e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
