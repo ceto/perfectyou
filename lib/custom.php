3585a9fbae4841cd867ace0b94967a1456d92fac
Szabó Gábor <szabogabi@gmail.com>
2014-06-19 12:04:40 +0200

    Tax Meta Class added

--- lib/custom.php
+++ lib/custom.php
@@ -166,6 +166,54 @@ function cmb_initialize_cmb_meta_boxes() {
 
 }
 
+/******** Category Taxonomy Meta Boxes Definition *********/
+require_once("Tax-meta-class/Tax-meta-class.php");
+if (is_admin()){
+  /* 
+   * prefix of meta keys, optional
+   */
+  $prefix = 'ba_';
+  /* 
+   * configure your meta box
+   */
+  $config = array(
+    'id' => 'category_meta',          // meta box id, unique per meta box
+    'title' => 'Category Meta Box',          // meta box title
+    'pages' => array('category'),        // taxonomy name, accept categories, post_tag and custom taxonomies
+    'context' => 'normal',            // where the meta box appear: normal (default), advanced, side; optional
+    'fields' => array(),            // list of meta fields (can be added by field arrays)
+    'local_images' => false,          // Use local or hosted images (meta box images for add/remove)
+    'use_with_theme' => true          //change path if used with theme set to true, false for a plugin or anything else for a custom path(default false).
+  );
+  
+  
+  /*
+   * Initiate your meta box
+   */
+  $cat_meta =  new Tax_Meta_Class($config);
+  
+  /*
+   * Add fields to your meta box
+   */
+  
+  //Image field
+  $cat_meta->addImage($prefix.'image_field_id',array('name'=> __('Fullscreen image(min:1600×1024px)','roots')));
+
+  //file upload field
+  //$repeater_fields[] = $my_meta->addImage($prefix.'image_field_id',array('name'=> __('My Image ','tax-meta')),true);
+  
+  /*
+   * Then just add the fields to the repeater block
+   */
+  //repeater block
+  $cat_meta->addRepeaterBlock($prefix.'re_',array('inline' => true, 'name' => __('This is a Repeater Block','tax-meta'),'fields' => $repeater_fields));
+  
+  /*
+   * Don't Forget to Close up the meta box decleration
+   */
+  //Finish Meta Box Decleration
+  $cat_meta->Finish();
+}
 
 
 
