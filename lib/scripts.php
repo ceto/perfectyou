987e491d0485e59010fd4e02c0ba62c79876636f
Szabó Gábor <szabogabi@gmail.com>
2014-06-24 17:49:57 +0200

    Home slogan logic

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2471a26c493ade29e32fa3592a03bdae');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b2376ca1ed58ea95c6fb870490b3d314');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- template-home.php
+++ template-home.php
@@ -4,13 +4,15 @@ Template Name: Homepage Template
 */
 ?>
 <header id="homeh" class="home-header fullscreen">
-    <div class="homeh-inner shape" style="margin-top:1000px;">
-      <h1 class="home-title"><?php the_title(); ?></h1>
-      <?php if ( get_post_meta( $post->ID, '_meta_lead', TURE ) ): ?>
-        <div class="home-lead"><?php echo get_post_meta( $post->ID, '_meta_lead', TURE );  ?></div>
-      <?php endif ?>
+    <div class="homeh-inner shape2" style="margin-top:1000px;">
+      <h1 class="home-title">Plasztikai sebészeti kezelések, esztétikai- és szépészeti beavatkozások</h1>
+      <?php //if ( get_post_meta( $post->ID, '_meta_lead', TURE ) ): ?>
+        <div class="home-lead">
+          <?php // echo get_post_meta( $post->ID, '_meta_lead', TURE );  ?>
+        </div>
+      <?php //endif ?>
       <div class="home-headaction">
-        <a class="btn" href="?cat=33"><?php _e('Ugrok a kezelésekre','root') ?></a>
+        <a class="btn btn-filled" href="#why-us"><?php _e('Ugrok a kezelésekre','root') ?></a>
       </div>
     </div>
 </header>
@@ -47,7 +49,7 @@ Template Name: Homepage Template
     </blockquote>
   
     <p>A folyamatos szakmai képzés elengedhetetlen ahhoz, hogy a nívós szaktudásnak köszönhetően a lehető legjobb ellátást nyújtsuk, ezért vagyunk elkötelezettek amellett, hogy pácienseinknek mindezt kizárólag up-to-date technikákkal, kiváló eszközökkel, technológiákkal és a legjobb minőségű alapanyagokkal biztosítsuk.</p>
-    <a href="#" class="btn btn-filled">Bővebben</a>
+    <a href="#" class="btn">Bővebben</a>
    </div>
 </section>
 
@@ -58,7 +60,7 @@ Template Name: Homepage Template
     <a href="#contact" data-toggle="collapse" class="btn">Jelentkezem</a>
   </div>
 </section>
-<section class="why-us fullscreen">
+<section id="why-us" class="why-us fullscreen">
   <div class="whu-inner">
     
     <ul id="myTab" class="nav nav-tabs">
@@ -71,14 +73,17 @@ Template Name: Homepage Template
       <div class="tab-pane fade active in" id="fiatal">
         <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. </p>
         <p>Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
+        <a href="?cat=35" class="btn">További beavatkozások</a>
       </div>
       <div class="tab-pane fade" id="szep">
         <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee.</p>
         <p>Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</p>
+        <a href="?cat=36" class="btn">Még több szépítő beavatkozások</a>
       </div>
       <div class="tab-pane fade" id="mutet">
         <p>Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony.</p>
         <p>Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer blog stumptown. Pitchfork sustainable tofu synth chambray yr.</p>
+        <a href="?cat=37" class="btn">Mutasd mindet</a>
       </div>
     </div>
 
