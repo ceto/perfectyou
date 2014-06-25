ebaa32cb4d4ba8bd4652035e761d63f3088044d0
Szabó Gábor <szabogabi@gmail.com>
2014-06-25 14:55:40 +0200

    home kezeleslist

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b2376ca1ed58ea95c6fb870490b3d314');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'dfc2232f138818a39ea69d25a056a374');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- template-home.php
+++ template-home.php
@@ -12,7 +12,7 @@ Template Name: Homepage Template
         </div>
       <?php //endif ?>
       <div class="home-headaction">
-        <a class="btn btn-filled" href="#why-us"><?php _e('Ugrok a kezelésekre','root') ?></a>
+        <a class="btn btn-filled" href="#feat-treats"><?php _e('Ugrok a kezelésekre','root') ?></a>
       </div>
     </div>
 </header>
@@ -60,30 +60,117 @@ Template Name: Homepage Template
     <a href="#contact" data-toggle="collapse" class="btn">Jelentkezem</a>
   </div>
 </section>
-<section id="why-us" class="why-us fullscreen">
-  <div class="whu-inner">
+<section id="feat-treats" class="feat-treats fullscreen">
+  <div class="ftreats-inner">
     
     <ul id="myTab" class="nav nav-tabs">
       <li class="active"><a href="#fiatal" data-toggle="tab">Fiatalodni szeretne?</a></li>
       <li class=""><a href="#szep" data-toggle="tab">Szépülni szeretne?</a></li>
-      <li class=""><a href="#mutet" data-toggle="tab">Műtét nélkül</a></li>
+      <li class=""><a href="#fan" data-toggle="tab">Páciensek kedvencei</a></li>
     </ul>
     
     <div id="myTabContent" class="tab-content">
-      <div class="tab-pane fade active in" id="fiatal">
-        <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. </p>
-        <p>Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
-        <a href="?cat=35" class="btn">További beavatkozások</a>
+      <div class="tab-pane fade active in tab-fiatal" id="fiatal">
+        <?php 
+          $saved_data = get_tax_meta(6,'ba_image_field_id');
+          $cat_img_id=$saved_data['id'];
+          $imoci=wp_get_attachment_image_src( $cat_img_id, 'full43');
+        ?>
+        <style>
+          .feat-treats .tab-fiatal{
+            background-image:url('<?php echo $imoci[0]; ?>');
+          }
+        </style>
+        <?php
+          $the_kezeles = new WP_Query(array(
+            'post_type' => array('kezeles'),
+            'cat'  => 35,
+            'posts_per_page' => -1,
+          ));
+        ?>
+
+        <?php if ($the_kezeles->have_posts()) : ?>
+        <section class="kezeleslist">
+          <div class="kezeleslist-inner">
+            <h2><?php _e('Fiatalító műtétek és beavatkozások','roots') ?></h2>
+            <?php while ($the_kezeles->have_posts()) : $the_kezeles->the_post(); ?>
+              <?php if (get_post_type()=='kezeles'): ?>
+                  <?php get_template_part('templates/item', get_post_type()); ?>
+              <?php endif ?>
+            <?php endwhile; ?>
+            <a href="?cat=35" class="btn">További beavatkozások</a>
+          </div>
+        </section>
+        <?php endif; ?>
+        
       </div>
-      <div class="tab-pane fade" id="szep">
-        <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee.</p>
-        <p>Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</p>
-        <a href="?cat=36" class="btn">Még több szépítő beavatkozások</a>
+      <div class="tab-pane fade tab-szep" id="szep">
+        <?php 
+          $saved_data = get_tax_meta(36,'ba_image_field_id');
+          $cat_img_id=$saved_data['id'];
+          $imoci=wp_get_attachment_image_src( $cat_img_id, 'full43');
+        ?>
+        <style>
+          .feat-treats .tab-szep{
+            background-image:url('<?php echo $imoci[0]; ?>');
+          }
+        </style>
+        <?php
+          $the_kezeles = new WP_Query(array(
+            'post_type' => array('kezeles'),
+            'cat'  => 36,
+            'posts_per_page' => -1,
+          ));
+        ?>
+
+        <?php if ($the_kezeles->have_posts()) : ?>
+        <section class="kezeleslist">
+          <div class="kezeleslist-inner">
+            <h2><?php _e('Szépítő kezelések és esztétikai beavatkozások','roots') ?></h2>
+            <?php while ($the_kezeles->have_posts()) : $the_kezeles->the_post(); ?>
+              <?php if (get_post_type()=='kezeles'): ?>
+                  <?php get_template_part('templates/item', get_post_type()); ?>
+              <?php endif ?>
+            <?php endwhile; ?>
+            <a href="?cat=36" class="btn">Még több szépítő beavatkozások</a>
+          </div>
+        </section>
+        <?php endif; ?>
       </div>
-      <div class="tab-pane fade" id="mutet">
-        <p>Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony.</p>
-        <p>Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer blog stumptown. Pitchfork sustainable tofu synth chambray yr.</p>
-        <a href="?cat=37" class="btn">Mutasd mindet</a>
+      
+      <div class="tab-pane fade tab-fan" id="fan">
+        <?php 
+          $saved_data = get_tax_meta(33,'ba_image_field_id');
+          $cat_img_id=$saved_data['id'];
+          $imoci=wp_get_attachment_image_src( $cat_img_id, 'full43');
+        ?>
+        <style>
+          .feat-treats .tab-fan{
+            background-image:url('<?php echo $imoci[0]; ?>');
+          }
+        </style>
+        <?php
+          $the_kezeles = new WP_Query(array(
+            'post_type' => array('kezeles'),
+            'cat'  => 33,
+            'posts_per_page' => 4,
+          ));
+        ?>
+
+        <?php if ($the_kezeles->have_posts()) : ?>
+        <section class="kezeleslist">
+          <div class="kezeleslist-inner">
+            <h2><?php _e('Pácienseink kedvencei','roots') ?></h2>
+            <?php while ($the_kezeles->have_posts()) : $the_kezeles->the_post(); ?>
+              <?php if (get_post_type()=='kezeles'): ?>
+                  <?php get_template_part('templates/item', get_post_type()); ?>
+              <?php endif ?>
+            <?php endwhile; ?>
+            <a href="?cat=33" class="btn">Mutasd mindet</a>
+          </div>
+        </section>
+        <?php endif; ?>
+
       </div>
     </div>
 
