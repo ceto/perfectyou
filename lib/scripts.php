f11502cbe2a582b10a45ad991d98e631fe5520ae
Szabó Gábor <szabogabi@gmail.com>
2014-06-25 16:29:56 +0200

    illlnav sepia

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fb5fd2fd3d21df1c741cee0796da9894');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5c84f0e080926fd16e8e3d0d58fad43e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
