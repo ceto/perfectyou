ca4f7e4f94070e5db6b050673408b37d8505d28a
Szabó Gábor <szabogabi@gmail.com>
2014-06-26 14:44:54 +0200

    callme

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e6f5f0d009c0b9583f4437d6bbdb0010');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '64429b6a237f33960e66f8c2ad4b020a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- template-home.php
+++ template-home.php
@@ -36,9 +36,13 @@ Template Name: Homepage Template
 
 <section class="callme">
   <div class="callme-inner">
-    <h3>Jelentkezzen konzultációra</h3>
-    <p>Weblapon történő jelentkezés esetén az első konzultáció ingyenes</p>
-    <a href="#contact" data-toggle="collapse" class="btn">Jelentkezem</a>
+    <h3>Felkészült a konzultációra?</h3>
+    <p class="vege"><i class="ss-glyphish-filled ss-phone telicon"></i> Hívjon
+      <a href="tel:+36707705653" class="phone">+36.70.570.5653</a><span class="or">– vagy –</span>
+      <a href="#contact" data-toggle="collapse" class="btn btn-filled online">
+        <i class="ss-glyphish-filled ss-write"></i> Jelentkezzen online
+      </a>
+    </p>
   </div>
 </section>
 
