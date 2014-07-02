6ecdbf4ce2580e6d72fe0727266617ba73ff0655
Szabó Gábor <szabogabi@gmail.com>
2014-07-02 17:46:20 +0200

    blog + page header tweak

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8cdbdd692569aa18abfe13f5ee522892');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0493f87e8eed7a1508b8dc3c9303192e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
