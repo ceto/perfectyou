9895ae41fd6c2e84475f2f04ca96e9ad84d68b21
Szabó Gábor <szabogabi@gmail.com>
2014-07-16 17:00:15 +0200

    lablec signo

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd441e9a98bdd3f5ef6e231041dd9fcc5');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'cff5dae927d46f308feebd62624e6367');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
