dc7bd5931618494c48e9815067845bb8f2f36b8a
Szabó Gábor <szabogabi@gmail.com>
2014-07-18 17:19:58 +0200

    treat head resize off 2

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e0d433c69ef2483f5741f7c8eb05f509');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c12638673868c9c9e2ffa1531be98236');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
