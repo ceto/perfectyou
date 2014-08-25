5caa4afb8b6be5eec095d77188780769ee0579f9
Gabor Szabo <szabogabi@gmail.com>
2014-08-25 14:25:50 +0200

    kiskezeleskockak javitva2

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '04abb1c2cfaf9d37ae9a3b1abad3fe79');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd59a9b27b6808b7c79658d4b8a6a3e48');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
