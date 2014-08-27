9792d3acd88c5c2ebc5fe39c5b76ceb064ab809e
Gabor Szabo <szabogabi@gmail.com>
2014-08-27 17:16:07 +0200

    subsec head proba

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f210fe4d6fe7b4ef1f7983e0933cd18a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e6bba6728e47dcf3296d9f2cae38bac5');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
