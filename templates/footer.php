0610354dc61818a2dc3795bd115b9410e6609630
Gabor Szabo <szabogabi@gmail.com>
2015-01-14 10:47:52 +0100

    UA tracking & footerlilink  added

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,7 @@
 <footer class="content-info" role="contentinfo">
 	<?php dynamic_sidebar('sidebar-footer'); ?>
-	<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+	<p>&copy; <?php echo date('Y'); ?> All Rights Reserved &middot; <?php bloginfo('name'); ?> </p>
+	<p class="madeby">Handmade with Love by <a href="http://hydrogene.hu" target="_blank">Hydrogene</a></p>
 </footer>
 <?php //get_template_part('templates/call','me' ); ?>
 
