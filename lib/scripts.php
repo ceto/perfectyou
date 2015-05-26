b58b64af1e75869fd68e9968b2a2cfab85fe11d6
Gabor Szabo <szabogabi@gmail.com>
2015-05-26 13:27:50 +0200

    lablec és max width figure

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2a6ccc1492a7dadd1030c8e7d05e15d8');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '69bc141d37559f188fb662fca4f528de');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/sidebar.php
+++ templates/sidebar.php
@@ -27,26 +27,28 @@
 					<h4>Konzultáció és rendelés*</h4>
 					<p><strong>dr. Bulyovszky István</strong><br>
 					1106 Budapest, Örs vezér tere 25.<br>Árkád I. Irodaház 3. emelet<br>
-					<small>*bejelentkezést követően<br><a href="#">Térkép</a></small></p>
-					<p>1118 Budapest, Számadó u. 6.<br>
-					<small>*bejelentkezést követően<br><a href="#">Térkép</a></small></p>
-					<p><strong>Bejelentkezés telefonon<br>
-					<a href="tel:+36302991122">(+36) 30 299 1122</a></strong><br>
-					<small><a href="<?php echo get_permalink('2155'); ?>">vagy online</a></small></p>
+					<small>*bejelentkezést követően<br><a target="_blank" href="https://www.google.hu/maps/search/1106+Budapest,+%C3%96rs+vez%C3%A9r+tere+25.+%C3%81rk%C3%A1d+Irodah%C3%A1z+3.+emelet/@47.5028225,19.138501,17z/data=!3m1!4b1">Térkép</a></small></p>
+					<p><strong>Szt. Lukács Magánkórház</strong><br>
+					1139 Budapest, Kartács u. 1-5.<br>
+					<small><a target="_blank" href="https://www.google.hu/maps/place/Kart%C3%A1cs+u.+1,+Budapest,+1139/@47.529471,19.074451,17z/data=!3m1!4b1!4m2!3m1!1s0x4741db936ac9e1a7:0x8aa7abadb1034c55">Térkép</a></small></p>
+
 				</div>
 				<div class="felezve">
 					<h4>Műtétek helyszíne</h4>
 					<p><strong>Szt. Lukács Magánkórház</strong><br>
 					1139 Budapest, Kartács u. 1-5.<br>
-					<small><a href="#">Térkép</a></small></p>
+					<small><a target="_blank" href="https://www.google.hu/maps/place/Kart%C3%A1cs+u.+1,+Budapest,+1139/@47.529471,19.074451,17z/data=!3m1!4b1!4m2!3m1!1s0x4741db936ac9e1a7:0x8aa7abadb1034c55">Térkép</a></small></p>
 
-					<p><strong>Kútvölgyi Klinikai Tömb</strong><br>
+<!--					<p><strong>Kútvölgyi Klinikai Tömb</strong><br>
 					1125 Budapest, Kútvölgyi út 4., 5. em.<br>
-					<small><a href="#">Térkép</a></small></p>
+					<small><a target="_blank" href="https://www.google.hu/maps/place/K%C3%BAtv%C3%B6lgyi+%C3%BAt+4,+Budapest,+Semmelweis+Egyetem,+1125/@47.511017,19.005381,17z/data=!3m1!4b1!4m2!3m1!1s0x4741debec3c303c7:0x782481a88fd5c519">Térkép</a></small></p>
 
 					<p><strong>Királyerdei Szépségközpont</strong><br>
 					1213 Budapest, Szent István út 248-250.<br>
-					<small><a href="#">Térkép</a></small></p>
+					<small><a target="_blank" href="https://www.google.hu/maps/place/Szent+Istv%C3%A1n+%C3%BAt+250,+Budapest,+1213/@47.3984135,19.098556,17z/data=!3m1!4b1!4m2!3m1!1s0x4741e867eb9f5ee5:0xbc09f2053f86a70d">Térkép</a></small></p> -->
+					<p><strong>Bejelentkezés telefonon<br>
+					<a href="tel:+36302991122">(+36) 30 299 1122</a></strong><br>
+					<small><a href="<?php echo get_permalink('810'); ?>/#contactblock">vagy online</a></small></p>
 				</div>
 
 			</div>
@@ -70,9 +72,9 @@
 <aside class="sidebar-search" role="complementary">
   <div class="sidebar-inner">
   <div class="social-icons">
-  	<a href="https://hu.linkedin.com/pub/istvan-bulyovszky/38/264/a46" target="_blank" title="LinkedIn"><i class="typcn typcn-social-linkedin-circular"></i></a>
   	<a href="https://www.facebook.com/DrBulyovszkyIstvanPlasztikaiSebesz" target="_blank" title="Facebook"><i class="typcn typcn-social-facebook-circular"></i></a>
-  	<a href="https://plus.google.com/+BulyovszkyIstván/" target="_blank" title="Google+"><i class="typcn typcn-social-google-plus-circular"></i></a>  	
+  	<a href="https://plus.google.com/+BulyovszkyIstván/" target="_blank" title="Google+"><i class="typcn typcn-social-google-plus-circular"></i></a>
+  	<a href="https://hu.linkedin.com/pub/istvan-bulyovszky/38/264/a46" target="_blank" title="LinkedIn"><i class="typcn typcn-social-linkedin-circular"></i></a>  	
   </div>
     <?php get_search_form(); ?>
   </div>
